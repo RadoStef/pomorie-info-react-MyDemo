505bf8e4ae2e5235221f2f2817f421a55afdedfa
RadoStef <rs_perfect@mail.bg>
2023-06-27 15:58:48 +0300

    background gradient and box shaddow added to h1 typewriter text

--- src/components/WelcomeSection/WelcomeSection.js
+++ src/components/WelcomeSection/WelcomeSection.js
@@ -4,13 +4,25 @@ import Box from '@mui/material/Box';
 import { styles } from './styles';
 import { TypewriterText } from './TypewriterText';
 
-export const WelcomeSection = ({text}) => {
-
+export const WelcomeSection = ({ text }) => {
   return (
     <Container sx={styles.container} maxWidth='xl'>
       <Box sx={styles.boxStyle}>
-        <h1 style={{fontSize: '28px', color: 'black', background: '#f1f1f1', padding: '10px', transform: 'skew(-15deg)'}}><TypewriterText text='Нещата за Поморие, които и GOOGLE не знае.'/></h1>
+        <h1
+          style={{
+            fontSize: '28px',
+            color: 'black',
+            background: '#f1f1f1',
+            padding: '20px',
+            transform: 'skew(-15deg)',
+            backgroundImage:
+              'linear-gradient(to right top, #b9c4d4, #bed5dd, #cbe4e2, #dff1e6, #f8fded)',
+            boxShadow: '4px 5px 9px -4px rgba(0,0,0,0.64)',
+          }}
+        >
+          <TypewriterText text='Нещата за Поморие, които и GOOGLE не знае.' />
+        </h1>
       </Box>
     </Container>
-  )
+  );
 };
